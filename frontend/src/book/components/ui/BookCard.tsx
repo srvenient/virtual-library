52d24579e44dfcca520191212199f97f76840867
harol <hamorales@unimonserrate.edu.co>
2025-04-27 10:37:06 -0500

    feat: Add Room and Computer APIs
    - Add Room API with CRUD operations
    - Add Computer API with CRUD operations
    - Follow project conventions and best practices
    - Use SQLModel for models and validation
    - Add proper error handling

--- frontend/src/book/components/ui/BookCard.tsx
+++ frontend/src/book/components/ui/BookCard.tsx
@@ -12,12 +12,12 @@ export default function BookCard({book}: { book: Book }) {
             />
             <div className="flex flex-col justify-between flex-1 px-4 py-3">
                 <div>
-                    <h1 className="text-lg font-medium text-theme-gray-darkest line-clamp-2">
+                    <h1 className="text-lg font-medium text-theme-gray-darkest line-clamp-1">
                         {book.title}
                     </h1>
                     <p className="text-sm text-theme-gray-medium">por {book.author}</p>
                 </div>
-                <button className="bg-theme-royal-blue w-fit p-2 pl-3 pr-3 mt-4 text-left hover:bg-theme-blue-light transition duration-200 hover:brightness-110">
+                <button className="bg-theme-royal-blue rounded-lg w-fit p-2 pl-3 pr-3 mt-4 text-left hover:bg-theme-blue-light transition duration-200 hover:brightness-110">
                     <span className="text-[15px] text-white font-medium cursor-pointer">Reservar</span>
                 </button>
             </div>
